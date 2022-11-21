97ece272ef83ddbd82e1a452fe8000b772dae538
Mizna Uppal <miznauppal@gmail.com>
2022-11-21 21:54:54 +0000

    done

---
+++ client/js/vent.js
@@ -0,0 +1,65 @@
+const body = document.querySelector("body");
+const postButton = document.querySelector("#post-button");
+const form = document.getElementById("post-form");
+const cover = document.querySelector("main.inner");
+// const exitButton = document.querySelector('#exit-button');
+// const sendButton = document.querySelector('#send-button');
+const gifButton = document.querySelector("#btn-gif");
+let apiKey = `a1Gm9f8gJX62owmzVfR5PddN5zDlklJ5`;
+let search = document.querySelector("#search");
+let gifChange = document.querySelector("#btn-remove");
+
+
+gifChange.style.display = 'none';
+
+gifButton.addEventListener('click', (e) =>{
+    e.preventDefault()
+    // gifChange.style.display ='block'
+    gettingGiphy();
+})
+
+function gettingGiphy(){
+
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${search.value}&api_key=${apiKey}&rating=pg&limit=5`)
+    .then((response) => response.json())
+    .then((data) => {
+        let results = document.querySelector("#results");
+        let result = document.querySelector("#result");
+        data.data.forEach((obj) =>{
+            console.log(obj.images.downsized.url)
+            let img = document.createElement("img")
+            img.style.width = "60px";
+            img.style.height = '45px';
+            let body = document.querySelector("body")
+            img.src = obj.images.original.url
+            img.alt =  obj.title
+
+            results.append(img)
+            function addGif(e){ 
+                result.style.display ='block'
+                e.preventDefault()
+                let newImg = img
+                result.append(newImg)
+                results.style.display = 'none'
+            }
+            
+            img.addEventListener('click', addGif)
+
+            let remove = document.querySelector('#btn-remove')
+
+            remove.addEventListener('click', (e) =>{
+                e.preventDefault()
+                if(result.contains(img)){
+                    result.style.display = 'none'
+                    result.removeChild(img)
+                    results.style.display = 'flex'
+                    results.append(img)
+                }  
+               
+            })
+            
+        })
+        
+        search.value = ''
+    })
+}
