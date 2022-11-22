6064078122ad020d695ec4a2939a7da6ec4c53ff
Adam-Dowsett <78181424+Adam-Dowsett@users.noreply.github.com>
2022-11-22 21:21:58 +0000

    added the functionailty to show all data when coming onto the page

--- server/data.js
+++ server/data.js
@@ -13,7 +13,37 @@ userEntries = [
         },
         dnt: "yesterday",
         comments: ["this is so sad","i feel sorry for you"]
-    }
+    },
+    {
+        id: 2,
+        gif: "",
+        category: "general",
+        entry: "I just bored",
+        emoji: {
+            happy: 0,
+            amused: 0,
+            shocked: 0,
+            angry: 0,
+            sad:0
+        },
+        dnt: "today",
+        comments: ["lol", "Try being entertained????"]
+    },
+    {
+        id: 3,
+        gif: "",
+        category: "general",
+        entry: "Random data chicken nuggets mildred mildred",
+        emoji: {
+            happy: 0,
+            amused: 0,
+            shocked: 0,
+            angry: 0,
+            sad:0
+        },
+        dnt: "yesterday",
+        comments: ["this is so sad","i feel sorry for you"]
+    },
 ]
 
 module.exports = userEntries;
