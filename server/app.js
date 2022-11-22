6eb2f82c155f57ff986ba3750413646afcf5ef44
JLP2000 <josh15phan@gmail.com>
2022-11-22 15:20:02 +0000

    changes for heroku

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const entryRoutes = require("./routes/entries")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
     res.send("Vent");
